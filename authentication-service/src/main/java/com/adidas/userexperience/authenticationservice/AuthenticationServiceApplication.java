e060f413c2d94f724ca80449da3e4c977a931652
Nnadozie Omeonu <nnadozieome@gmail.com>
2021-04-18 06:17:37 +0100

    Implemented the authorization server

--- authentication-service/src/main/java/com/adidas/userexperience/authenticationservice/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/adidas/userexperience/authenticationservice/AuthenticationServiceApplication.java
@@ -2,8 +2,12 @@ package com.adidas.userexperience.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAuthorizationServer
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
