ab79b40a2d3f2c3f51f5f044a681333de8c38407
Nnadozie Omeonu <nnadozieome@gmail.com>
2021-04-18 05:31:53 +0100

    Microservice code and docker structure setup

--- subscription-service/src/main/java/com/adidas/userexperience/subscriptionservice/SubscriptionServiceApplication.java
+++ subscription-service/src/main/java/com/adidas/userexperience/subscriptionservice/SubscriptionServiceApplication.java
@@ -2,8 +2,10 @@ package com.adidas.userexperience.subscriptionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SubscriptionServiceApplication {
 
 	public static void main(String[] args) {
